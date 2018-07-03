982e67c088d2aaebf1344ec38fc6d77c182cce7f
Eduardojvr <eduardojvr10@gmail.com>
2018-07-03 11:18:00 -0300

    Commit inicial

---
+++ escape/Assets/Controles/Controles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controles : MonoBehaviour {
+
+    public Rigidbody2D ave;
+    public float y = 0;
+    public float x = -8;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Vertical"))	{
+            y += 100;
+            ave.AddForce(new Vector2(0, y));
+        }
+	}
+}
