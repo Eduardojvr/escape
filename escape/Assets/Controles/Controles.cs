9552b81e638b8d8c3b42555562858c1786ae0525
Eduardojvr <eduardojvr10@gmail.com>
2018-07-04 11:53:36 -0300

    Atualizando cenário e física

--- escape/Assets/Controles/Controles.cs
+++ escape/Assets/Controles/Controles.cs
@@ -17,24 +17,25 @@ public class Controles : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if(Input.GetButtonDown("frente")){
-            x += 300;
+            x += 150;
             ave.AddForce(new Vector2(x, 0));
             x = 0;
         }
         if (Input.GetButtonDown("tras")){
-            x -= 300;
+            x -= 150;
             ave.AddForce(new Vector2(x, 0));
             x = 0;
         }
         if(Input.GetButtonDown("cima")){
-            y += 300;
+            y += 150;
             ave.AddForce(new Vector2(0, y));
             y = 0;
         }
-        if (Input.GetButtonDown("baixo")){
-            y -= 300;
-            ave.AddForce(new Vector2(0, y));
-            y = 0;
-        }
+
+        // if (Input.GetButtonDown("baixo")){
+        //     y -= 300;
+        //     ave.AddForce(new Vector2(0, y));
+        //     y = 0;
+        // }
 	}
 }
