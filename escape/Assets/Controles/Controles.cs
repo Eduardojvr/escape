d67c9ff302f796778c22d1109a9854da694210f2
Eduardojvr <eduardojvr10@gmail.com>
2018-07-06 17:38:39 -0300

    Adicionando chuva de meteoros

--- escape/Assets/Controles/Controles.cs
+++ escape/Assets/Controles/Controles.cs
@@ -7,6 +7,9 @@ public class Controles : MonoBehaviour {
     public Rigidbody2D ave;
     public float y = 0;
     public float x = -8;
+    public Transform colisor;
+    public int posx;
+    public Rigidbody2D meteor;
 
 
 	// Use this for initialization
@@ -16,6 +19,14 @@ public class Controles : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(colisor.position.y < -6){
+            posx = UnityEngine.Random.Range(-9, 9);
+            colisor.position = new Vector3(posx, 5.7f, colisor.position.z);
+        }
+        if(meteor.gravityScale > 1){
+            meteor.gravityScale = 0;
+        }
+
         if(Input.GetButtonDown("frente")){
             x += 150;
             ave.AddForce(new Vector2(x, 0));
